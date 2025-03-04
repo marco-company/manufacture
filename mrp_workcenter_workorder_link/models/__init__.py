bfbfcfa9cb4b27fbe38c5542fe79087a8260dc0a
Florian da Costa <florian.dacosta@akretion.com>
2025-03-04 11:29:20 +0100

    [FIX] License is AGPL, fix inconsistencies in files

--- mrp_workcenter_workorder_link/models/__init__.py
+++ mrp_workcenter_workorder_link/models/__init__.py
@@ -1,4 +1,4 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import mrp_workcenter
 from . import mrp_workorder
