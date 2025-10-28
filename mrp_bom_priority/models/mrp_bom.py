f34c4b276aba01430554fa265a92458c87090826
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-10-28 22:36:36 +0100

    [IMP] mrp_bom_priority: respect inheritance, when overloading _mrp_find() function

--- mrp_bom_priority/models/mrp_bom.py
+++ mrp_bom_priority/models/mrp_bom.py
@@ -2,7 +2,6 @@
 # @author: Quentin DUPONT (quentin.dupont@grap.coop)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from collections import defaultdict
 
 from odoo import api, fields, models
 
@@ -10,43 +9,19 @@ from odoo import api, fields, models
 class MrpBom(models.Model):
     _inherit = "mrp.bom"
 
-    # Column Section
     priority = fields.Selection(
         [("0", "Low"), ("1", "Normal"), ("2", "High"), ("3", "Very High")],
         default="0",
         help="Helps prioritize BoM.",
     )
 
+    def search(self, args, **kwargs):
+        if self.env.context.get("mrp_bom_priority_change_order"):
+            kwargs["order"] = "priority DESC, sequence, product_id, id"
+        return super().search(args, **kwargs)
+
     @api.model
-    def _bom_find(self, products, picking_type=None, company_id=False, bom_type=False):
-        """Override native function to change order"""
-        bom_by_product = defaultdict(lambda: self.env["mrp.bom"])
-        products = products.filtered(lambda p: p.type != "service")
-        if not products:
-            return bom_by_product
-        domain = self._bom_find_domain(
-            products,
-            picking_type=picking_type,
-            company_id=company_id,
-            bom_type=bom_type,
-        )
-
-        # Change here
-        order = "priority DESC, sequence, product_id, id"
-
-        if len(products) == 1:
-            bom = self.search(domain, order=order, limit=1)
-            if bom:
-                bom_by_product[products] = bom
-            return bom_by_product
-
-        boms = self.search(domain, order=order)
-
-        products_ids = set(products.ids)
-        for bom in boms:
-            products_implies = bom.product_id or bom.product_tmpl_id.product_variant_ids
-            for product in products_implies:
-                if product.id in products_ids and product not in bom_by_product:
-                    bom_by_product[product] = bom
-
-        return bom_by_product
+    def _bom_find(self, args, **kwargs):
+        return super(
+            MrpBom, self.with_context(mrp_bom_priority_change_order=True)
+        )._bom_find(args, **kwargs)
