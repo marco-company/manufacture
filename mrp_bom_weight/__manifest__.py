6dc5a824af5da160773938d3fde9ceb9f8e9f133
Quentin Dupont <perso@quentindupont.fr>
2025-12-26 14:43:01 +0100

    Update mrp_bom_weight/__manifest__.py
    Co-authored-by: Simon Hick <simon_hick@hotmail.com>

--- mrp_bom_weight/__manifest__.py
+++ mrp_bom_weight/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "MRP BoM Weight",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.0",
     "category": "Manufacturing",
     "author": "GRAP, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture",
